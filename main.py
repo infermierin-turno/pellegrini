f20464ec05c1cfb2edbcbbe20976920a22ae0759
infermierin-turno <infermieri.net@gmail.com>
2026-08-29 09:04:19 +0200

    Update Supabase key in main.py
    Updated Supabase key while maintaining the URL.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from typing import List, Optional, Union
 app = FastAPI()
 
 SUPABASE_URL = "https://ruvdlcgsmtwszxsposjt.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dmRsY2dzbXR3c3p4c3Bvc2p0Iiwicm9sZSI6InJ1dmRsY2dzbXR3c3p4c3Bvc2p0Iiwicm9sZSI6InJ1dmRsY2dzbXR3c3p4c3Bvc2p0Iiwicm9sZSI6InJ1dmRsY2dzbXR3c3p4c3Bvc2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNjQ4MzksImV4cCI6MjA5ODc0MDgzOX0.V_nFon6WsICyaiiN1bujrg5P9ORKb8-L1eMBlCFKZF8"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dmRsY2dzbXR3c3p4c3Bvc2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNjQ4MzksImV4cCI6MjA5ODc0MDgzOX0.V_nFon6WsICyaiiN1bujrg5P9ORKb8-L1eMBlCFKZF8"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Configurazione SMTP per l'invio delle email
