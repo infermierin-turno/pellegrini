e40f62c48a6da2e2a5d8f6599a38d266ca1114e5
infermierin-turno <infermieri.net@gmail.com>
2026-09-22 17:50:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 app.mount("/shopify", shopify_app)
 
 SUPABASE_URL = "https://ruvdlcgsmtwszxsposjt.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dmRsY2gs..."
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dmRsY2dzbXR3c3p4c3Bvc2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNjQ4MzksImV4cCI6MjA5ODc0MDgzOX0.V_nFon6WsICyaiiN1bujrg5P9ORKb8-L1eMBlCFKZF8"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
